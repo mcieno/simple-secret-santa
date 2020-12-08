43bbc0358091ff97f18962e3e804af600965afe4
mcieno <cieno.marco@gmail.com>
2020-12-08 23:45:32 +0100

    feat: create secret-santa application

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <Logo />
+    <Reveal />
+    <div class="block"></div>
+</template>
+
+<script>
+import Logo from './components/Logo.vue'
+import Reveal from './components/Reveal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Reveal,
+    Logo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #efefef;
+  padding-top: 60px;
+}
+
+html {
+  background-color: #AB2328;
+}
+</style>
+
+<style scoped>
+.block {
+  padding-bottom: 10vh;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="columns is-mobile is-centered">
+    <div class="column is-one-third">
+        <figure class="image">
+          <img alt="Secret Santa" src="../assets/logo.png">
+        </figure>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    msg: String
+  }
+}
+</script>
